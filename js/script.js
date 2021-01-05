626743e70e813008c225d04779a30fe9252cdcdc
Domenic Beall <domenicbeall2@gmail.com>
2021-01-05 19:52:01 +1100

    Add JQuery and added functionality to get weather data for selected date

--- js/script.js
+++ js/script.js
@@ -1,14 +1,49 @@
 const weatherAPIKey = "5ef88dc51d3ee5b81c89c1e9a4e916a9";
 
-getWeatherInformation("London");
+// This event is triggered when somebody selects a new date
+document.querySelector("#reservation-date").addEventListener('change', function (event){
+    // Get the selected date as a UNIX timestamp
+    var selectedDateUnix = event.target.valueAsNumber;
+
+    // Get weather information for the selected date
+    getWeatherInformation(selectedDateUnix);
+});
+
 
 // This function will get weather information for the city passed to it
-function getWeatherInformation(cityName) {
-
+function getWeatherInformation(UnixDate) {
     var weatherAPIurl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + 37.8136 + "&lon=" + 144.9631 + "&exclude=current,minutely,hourly,alerts&appid=" + weatherAPIKey;
 
+    // Convert the date input to a date time and add 2 hours
+    var selectedDate = new Date(UnixDate);
+    selectedDate.addHours(2);
+
     $.ajax(weatherAPIurl).then(function (response) {
-        console.log(response);
+        // Stores the weather data for the date the user selected if it is found
+        var selectedDateData = null;
+
+        // Go through the next 7 days weather data and check if any of them are equal to the selected date
+        for (let index = 0; index < response.daily.length; index++) {
+            const day = response.daily[index];
+            var date = new Date(day.dt * 1000);    
+            
+            if (date.getTime() == selectedDate.getTime()) {
+                selectedDateData = response.daily[index];
+            }
+        }
+
+        // If weather data was found for the selected date...
+        if (selectedDateData != null) {
+            console.log("We found weather information for the selected date!");
+        } else {
+            console.log("Could not find weather information for the selected date!");
+        }
     });
 
 }
+
+// Adding to the functionality of the date class so I can conveniently add 2 hours
+Date.prototype.addHours = function(h) {
+    this.setTime(this.getTime() + (h*60*60*1000));
+    return this;
+  }
